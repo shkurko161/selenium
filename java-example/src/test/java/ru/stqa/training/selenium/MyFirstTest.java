5cae2d3f63ed847fafadd1049e4a2bd65763302c
shkurko161 <shkurko.vasilij@yandex.ru>
2019-02-11 23:54:55 +0300

    Завершение 11.02

--- java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -1,3 +1,5 @@
+package ru.stqa.training.selenium;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -5,10 +7,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.concurrent.TimeUnit;
-
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
 public class MyFirstTest {
 
     private WebDriver driver;
