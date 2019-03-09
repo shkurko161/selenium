a4c8ddaae6226c64f271a0bf7d7763c92a386003
shkurko161 <shkurko.vasilij@yandex.ru>
2019-03-09 19:27:08 +0300

    Вторая часть 9го задания

--- java-example/src/test/java/ru/stqa/training/selenium/CountriesAndGeoZones.java
+++ java-example/src/test/java/ru/stqa/training/selenium/CountriesAndGeoZones.java
@@ -68,7 +68,26 @@ public class CountriesAndGeoZones extends TestBase {
         driver.findElement(By.name("username")).sendKeys(LogPass);
         driver.findElement(By.name("password")).sendKeys(LogPass);
         driver.findElement(By.name("login")).click();
-
+        List<WebElement> rows = driver.findElements(By.cssSelector("tr[class='row']"));
+        List<String> idCountry = new ArrayList<>();
+        for (WebElement r : rows) {
+            String id = r.findElement(By.cssSelector("td:nth-child(2)")).getAttribute("outerText");
+            idCountry.add(id);
+        }
+        for (String c : idCountry) {
+            driver.navigate().to(String.format("http://localhost/litecart/admin/?app=geo_zones&doc=edit_geo_zone&page=1&geo_zone_id=%s", c));
+            List<WebElement> geoRows = driver.findElements(By.cssSelector("table#table-zones > tbody > tr"));
+            List<String> geoTitle = new ArrayList<>();
+            geoRows.remove(geoRows.size()-1);
+            // i = 1, чтобы исключить header
+            for (int i = 1; i < geoRows.size(); i++) {
+                geoTitle.add(geoRows.get(i).findElement(By.cssSelector("td:nth-child(3) > select > option[selected='selected']")).getText());
+            }
+            List<String> geoSorted = geoTitle;
+            Collections.sort(geoSorted);
+            Assert.assertTrue(geoTitle.equals(geoSorted));
+            driver.navigate().back();
+        }
     }
 }
 
